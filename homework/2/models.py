79f9df20de2817c308375b5fdbb172265ba42f35
Dimitri Lezcano <dlezcan1@jh.edu>
2021-10-06 12:02:29 -0400

    HW2: Completed Log. Reg. with L2 Regularization

--- homework/2/models.py
+++ homework/2/models.py
@@ -94,12 +94,12 @@ class MCLogistic( MCModel ):
         # fit logistic regression
         while True:
             # conditional probability of Y_i given X and weights
-            p_yi1_g_AX = self._conditional_prob_yi( AX ).reshape( -1, 1 )
+            p_yi1_g_X = self._conditional_prob_yi( X ).reshape( -1, 1 )
 
             # calculate gradient and Hessian
-            grad_h = np.sum( AXh * (y - p_yi1_g_AX), axis=0 )
+            grad_h = np.sum( Xh * (y - p_yi1_g_X), axis=0 )
 
-            hess_h = np.sum( np.einsum( 'ij,ik->ijk', AXh, -AXh ) * (p_yi1_g_AX * (1 - p_yi1_g_AX)).reshape( -1, 1, 1 ),
+            hess_h = np.sum( np.einsum( 'ij,ik->ijk', Xh, -Xh ) * (p_yi1_g_X * (1 - p_yi1_g_X)).reshape( -1, 1, 1 ),
                              axis=0 )
 
             # # brute force method
@@ -117,7 +117,6 @@ class MCLogistic( MCModel ):
             # check for update size small enough to break
             update_norm = np.linalg.norm( update )
             if update_norm <= tol:
-                print()
                 break
 
             # if
@@ -162,25 +161,69 @@ class MCLogisticWithL2( MCModel ):
 
         self.W = np.random.randn( nfeatures + 1, 1 )  # last row is the bias term
         self.W /= 1e5 * np.abs( self.W ).max()
+        self.l2penalty = 0.001
 
     def fit( self, *, X, y, A, tol ):
         # standardize the forms
         X = self._fix_test_feats( X )
-        Xh = np.hstack( (X, np.ones( (X.shape[ 0 ]), 1 )) )
+        Xh = np.hstack( (X.toarray(), np.ones( (X.shape[ 0 ], 1) )) )
         y = y.reshape( -1, 1 )
 
         # compute A(X)
         AX = A( X )
-        AXh = np.hstack( (AX, np.ones( (X.shape[ 0 ], 1) )) )
+        AXh = np.hstack( (AX.toarray(), np.ones( (X.shape[ 0 ], 1) )) )
 
-        # TODO: fit function (L2)
-        raise NotImplementedError()
+        # fit logistic regresstion (L2 regularization)
+        while True:
+            # conditional probability of Y_i given X and weights
+            p_yi1_g_X = self._conditional_prob_yi( X ).reshape( -1, 1 )
+
+            # calculate gradient and Hessian
+            grad_h = np.sum( Xh * (y - p_yi1_g_X), axis=0 ).reshape( -1, 1 ) + self.l2penalty * self.W
+
+            hess_h = np.sum( np.einsum( 'ij,ik->ijk', Xh, -Xh ) * (p_yi1_g_X * (1 - p_yi1_g_X)).reshape( -1, 1, 1 ),
+                             axis=0 ) + self.l2penalty * np.eye( self.W.size )
+
+            # # brute force method
+            # hess_h2 = np.zeros_like( hess_h )
+            # for i in range( Xh.shape[ 0 ] ):
+            #     xh_i = Xh[ i ]
+            #     hess_h2 += np.outer( xh_i, xh_i ) * p_yi1_g_X[ i ] * (1 - p_yi1_g_X[ i ])
+            #
+            # # for
+
+            # determine update and update parameters
+            update = -(np.linalg.pinv( hess_h, hermitian=True ) @ grad_h)
+            self.W += update.reshape( -1, 1 )  # update the parameters
+
+            # check for update size small enough to break
+            update_norm = np.linalg.norm( update )
+            if update_norm <= tol:
+                break
+
+            # if
 
     # fit
 
     def predict( self, X ):
         X = self._fix_test_feats( X )
-        # TODO: predict function (L2)
-        predictions = np.zeros( (X.shape[ 0 ], 1), dtype=np.int )
+        prob_yi1 = self._conditional_prob_yi( X )
+        predictions = (prob_yi1 > 0.5).astype( int ).reshape( -1, 1 )
 
         return predictions
+
+    def _conditional_prob_yi( self, X ):
+        """ Compute p(Y_i | X; W) """
+        sig_exp = np.exp( -(X @ self.W[ :-1 ] + self.W[ -1 ]) )  # sigmoid exponential expression
+
+        # Binary implementation
+        p_yi_g_x = 1 / (1 + sig_exp)
+
+        # # Multi-class implementation
+        # p_yi_g_x = np.zeros_like( sig_exp )
+        # p_yi_g_x[ :, -1 ] = 1 / (1 + sig_exp[ :, :-1 ].sum( axis=1 ))
+        # p_yi_g_x[ :, :-1 ] = sig_exp[ :, :-1 ] * p_yi_g_x[ :, -1 ].reshape( -1, 1 )
+
+        return p_yi_g_x
+
+    # _conditional_prob_yi
